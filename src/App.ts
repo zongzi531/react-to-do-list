79ebf0a4b4c873922a6ae29d21ae80928a940e48
zongzi531 <zongzi.xy@gmail.com>
2018-04-29 17:51:50 +0800

    Init TypeScript Ver.
    # Init $ create-react-app my-app --scripts-version=react-scripts-ts
    # Use @types/node@9.6.7, fixed Duplicate identifier with @types/node@10.0
    # Issues URL: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/25342
    # Delete /docs and /src/**/*.js
    # Change js file to ts file, can't change index.tsx to index.ts
    # ADD TSLint Rules semicolon: https://palantir.github.io/tslint/rules/semicolon/

---
+++ src/App.ts
@@ -0,0 +1,18 @@
+import { Component, createElement as e } from 'react'
+// import Title from './Title'
+// import ListWrapper from './ListWrapper'
+// import { DragDropContext } from 'react-dnd'
+// import HTML5Backend from 'react-dnd-html5-backend'
+
+// @DragDropContext(HTML5Backend)
+export default class App extends Component<any, any> {
+  public render() {
+    return e(
+      'div',
+      { id: 'app', className: 'row'},
+      e('div', { className: 'col-md-4' }),
+      // e('div', { className: 'col-md-4' }, e(Title), e(ListWrapper)),
+      e('div', { className: 'col-md-4' })
+    )
+  }
+}
